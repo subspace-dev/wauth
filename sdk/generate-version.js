fe74e29faea4b718259025289caab18522836392
ankushKun <ankush4singh@gmail.com>
2025-07-23 18:11:33 +0530

    build updates

--- sdk/generate-version.js
+++ sdk/generate-version.js
@@ -2,4 +2,5 @@ import fs from 'fs';
 
 const packageJSON = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 
+console.log(`SDK version: ${packageJSON.version}`);
 fs.writeFileSync('./src/version.ts', `export const WAUTH_VERSION = "${packageJSON.version}";\n`);
